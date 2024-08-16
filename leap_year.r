e366ed048a8a23ee319e405dfad4decbe061e26e
SKoushick <86823538+SKoushick@users.noreply.github.com>
2024-08-16 14:52:46 +0530

    Create leap_year

---
+++ leap_year.r
@@ -0,0 +1,37 @@
+
+#a=readline(prompt="Enter the value for a:)
+#a=as.integer(a)
+
+#b=readline(prompt="Enter the value for b:")
+#b=as.integer(b)
+
+
+#if(a<b){
+ # cat("the value of a is smaller than b..")
+  
+#} else if(a>b) {
+#  cat("the value of a is greater than b..")
+#} else{
+#  cat("both the a and b are equal")
+
+
+#}
+
+
+d=readline(prompt="Enter the year:")
+d=as.integer(d)
+
+if(d%%4==0 ){
+  if(d%%100==0){
+    if(d%%400==0){
+  cat("leap year")
+    }
+    else{
+      cat("not a leap year")
+    }
+    }else{
+      cat("leap year")
+      }
+} else{
+  cat(paste(d,"this is not a leap year"))
+}
